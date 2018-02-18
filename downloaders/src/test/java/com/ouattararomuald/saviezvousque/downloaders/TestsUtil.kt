8aaa0d022b3aa1bd9c5d4162c57b10aaafa28f65
Romuald OUATTARA <ouattararomuald@gmail.com>
2018-02-18 16:06:15 +0100

    Remove XML related methods

--- downloaders/src/test/java/com/ouattararomuald/saviezvousque/downloaders/TestsUtil.kt
+++ downloaders/src/test/java/com/ouattararomuald/saviezvousque/downloaders/TestsUtil.kt
@@ -1,24 +1,17 @@
 package com.ouattararomuald.saviezvousque.downloaders
 
-import com.ouattararomuald.saviezvousque.common.Rss
 import java.io.File
-import org.simpleframework.xml.core.Persister
 
 
 class TestsUtil {
   companion object {
-    const val FEED_FILE_NAME = "/feed.xml"
+    const val CATEGORIES_JSON_FILE = "/categories.json"
+    const val POSTS_JSON_FILE = "/posts.json"
 
     @JvmStatic
     fun loadFileFromResources(filename: String): File {
       val url = TestsUtil::class.java.getResource(filename)
       return File(url.toURI())
-    }
-
-    @JvmStatic
-    fun deserializeXmlFromFile(source: File): Rss? {
-      val serializer = Persister()
-      return serializer.read(Rss::class.java, source)
     }
   }
 }
