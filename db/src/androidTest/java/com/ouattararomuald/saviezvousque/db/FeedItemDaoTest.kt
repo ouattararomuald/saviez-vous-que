0d02360504a0c9a9d038657d238415478970db78
Romuald OUATTARA <ouattararomuald@gmail.com>
2018-02-18 19:01:31 +0100

    Add Tests for FeedCategoryDao and FeedItemDao

---
+++ db/src/androidTest/java/com/ouattararomuald/saviezvousque/db/FeedItemDaoTest.kt
@@ -0,0 +1,151 @@
+package com.ouattararomuald.saviezvousque.db
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.persistence.room.Room
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.threeten.bp.OffsetDateTime
+
+@RunWith(AndroidJUnit4::class)
+class FeedItemDaoTest {
+
+  private lateinit var database: AppDatabase
+  private lateinit var feedItemDao: FeedItemDao
+
+  // Make sure that Room executes all the database operations instantly.
+  @JvmField
+  @Rule
+  val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+  init {
+    AndroidThreeTen.init(InstrumentationRegistry.getTargetContext())
+  }
+
+  @Before
+  fun setUp() {
+    createDatabase()
+  }
+
+  private fun createDatabase() {
+    val appContext = InstrumentationRegistry.getTargetContext()
+    database = Room.inMemoryDatabaseBuilder(appContext, AppDatabase::class.java)
+        .allowMainThreadQueries()
+        .build()
+    feedItemDao = database.feedItemDao()
+  }
+
+  @After
+  fun tearDown() {
+    database.close()
+  }
+
+  @Test
+  fun getAllFeedItems() {
+    val feedItems = insertFeedItems(quantity = 10)
+
+    feedItemDao.getAll()
+        .test()
+        .assertNoErrors()
+        .assertNotComplete()
+        .assertValueCount(1)
+        .assertValue(feedItems)
+  }
+
+  @Test
+  fun insertFeedItemShouldSuccess() {
+    val feedItem = createFeedItem()
+
+    feedItemDao.insert(listOf(feedItem))
+
+    feedItemDao.getAll()
+        .test()
+        .assertNoErrors()
+        .assertNotComplete()
+        .assertValue(listOf(feedItem))
+  }
+
+  @Test
+  fun insertDuplicateFeedItemShouldReplaceExistingOne() {
+    val feed = createFeedItem()
+
+    feedItemDao.insert(listOf(feed))
+    feedItemDao.insert(listOf(feed))
+
+    feedItemDao.getAll()
+        .test()
+        .assertNoErrors()
+        .assertNotComplete()
+        .assertValueCount(1)
+        .assertValue(listOf(feed))
+  }
+
+  @Test
+  fun deleteAllFeedItemsShouldSuccess() {
+    val feeds = insertFeedItems(quantity = 10)
+
+    feedItemDao.getAll()
+        .test()
+        .assertNoErrors()
+        .assertNotComplete()
+        .assertValueCount(1)
+        .assertValue(feeds)
+
+    feedItemDao.deleteAll()
+
+    feedItemDao.getAll()
+        .test()
+        .assertNoErrors()
+        .assertNotComplete()
+        .assertValue(emptyList())
+  }
+
+  private fun createFeedItem(id: Int = 1): FeedItem = FeedItem(
+      id,
+      "htt://fake-url-$id.jpeg",
+      OffsetDateTime.now(),
+      OffsetDateTime.now(),
+      "content"
+  )
+
+  /**
+   * Insert the given [quantity] of [FeedItem](s) into the database.
+   *
+   * Each feed will be saved with an id corresponding to its index + 1.
+   *
+   * For example:
+   * ```
+   * insertFeedItems(quantity = 2) // insert feeds: FeedItem(id=1,...) and FeedItem(id=2,...)
+   * ```
+   *
+   * @return a list containing the feeds inserted.
+   */
+  private fun insertFeedItems(quantity: Int = 1): List<FeedItem> {
+    if (quantity < 1) {
+      throw IllegalArgumentException("quantity must be greater or equals to 1")
+    }
+
+    val feeds = mutableListOf<FeedItem>()
+
+    (1..quantity).forEach { index ->
+      val feed = FeedItem(
+          index,
+          "htt://fake-url-$index.jpeg",
+          OffsetDateTime.now(),
+          OffsetDateTime.now(),
+          "content"
+      )
+
+      feedItemDao.insert(listOf(feed))
+
+      feeds.add(feed)
+    }
+
+    return feeds
+  }
+}
