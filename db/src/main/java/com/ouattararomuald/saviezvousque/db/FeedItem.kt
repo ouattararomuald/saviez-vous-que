ee7b4ef84b2ec8fa37bc4f31f850d6fa3376e72e
Romuald OUATTARA <ouattararomuald@gmail.com>
2018-02-18 19:00:03 +0100

    Makes database entities and daos public

--- db/src/main/java/com/ouattararomuald/saviezvousque/db/FeedItem.kt
+++ db/src/main/java/com/ouattararomuald/saviezvousque/db/FeedItem.kt
@@ -12,7 +12,7 @@ import org.threeten.bp.OffsetDateTime
   Index(value = ["publishedOn"]),
   Index(value = ["updatedOn"])
 ])
-internal data class FeedItem constructor(
+data class FeedItem constructor(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo val imageUrl: String,
     @ColumnInfo val publishedOn: OffsetDateTime,
