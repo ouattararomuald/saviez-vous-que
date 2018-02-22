01c3a4f4b7ddd4363398c301325bb34db9ee5488
Romuald OUATTARA <ouattararomuald@gmail.com>
2018-02-22 23:00:34 +0100

    Initialize fresco in Application

---
+++ app/src/main/java/com/ouattararomuald/saviezvousque/App.kt
@@ -0,0 +1,12 @@
+package com.ouattararomuald.saviezvousque
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+
+class App : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+    Fresco.initialize(this);
+  }
+}
