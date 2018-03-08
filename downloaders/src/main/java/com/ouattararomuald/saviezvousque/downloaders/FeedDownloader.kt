6253788fc88e41ec8095362de2f512f103255dd0
Romuald OUATTARA <ouattararomuald@gmail.com>
2018-03-08 15:42:51 +0100

    Replace Flowable with Single in DOwnloaders module

--- downloaders/src/main/java/com/ouattararomuald/saviezvousque/downloaders/FeedDownloader.kt
+++ downloaders/src/main/java/com/ouattararomuald/saviezvousque/downloaders/FeedDownloader.kt
@@ -2,30 +2,32 @@ package com.ouattararomuald.saviezvousque.downloaders
 
 import com.ouattararomuald.saviezvousque.common.Category
 import com.ouattararomuald.saviezvousque.common.Post
-import io.reactivex.Flowable
+import io.reactivex.Single
 
 /** Provides services that allow you to download feeds. */
 class FeedDownloader private constructor(private val feedService: FeedService) : FeedService {
 
   /** Returns the list of publications. */
-  override fun getPosts(): Flowable<List<Post>> = feedService.getPosts()
+  override fun getPosts(): Single<List<Post>> = feedService.getPosts()
 
   /** Returns the list of categories. */
-  override fun getCategories(): Flowable<List<Category>> = feedService.getCategories()
+  override fun getCategories(): Single<List<Category>> = feedService.getCategories()
 
   /**
    * Gets the post of the given [page].
    *
    * @param page The page for which you want to retrieve the list of posts.
    */
-  override fun getPostByPage(page: Int): Flowable<List<Post>> = feedService.getPostByPage(page)
+  override fun getPostByPage(page: Int): Single<List<Post>> = feedService.getPostByPage(page)
 
   /**
    * Gets the post of the given [categoryId].
    *
    * @param categoryId The category for which you want to retrieve the list of posts.
    */
-  override fun getPostsByCategory(categoryId: Int): Flowable<List<Post>> = feedService.getPostsByCategory(categoryId)
+  override fun getPostsByCategory(categoryId: Int): Single<List<Post>> {
+    return feedService.getPostsByCategory(categoryId)
+  }
 
   companion object Builder {
     /** Creates a new instance of [FeedDownloader]. */
