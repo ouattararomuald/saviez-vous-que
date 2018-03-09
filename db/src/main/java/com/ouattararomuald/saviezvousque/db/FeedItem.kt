e530e92a4394af5808d4cbc6819bdf8376294d0c
Romuald OUATTARA <ouattararomuald@gmail.com>
2018-03-09 20:50:05 +0100

    Display post from database

--- db/src/main/java/com/ouattararomuald/saviezvousque/db/FeedItem.kt
+++ db/src/main/java/com/ouattararomuald/saviezvousque/db/FeedItem.kt
@@ -3,18 +3,18 @@ package com.ouattararomuald.saviezvousque.db
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.Index
-import android.arch.persistence.room.PrimaryKey
 import org.threeten.bp.LocalDateTime
 
 
-@Entity(indices = [
+@Entity(primaryKeys = ["id", "categoryId"],
+    indices = [
   Index(value = ["imageUrl", "categoryId"], unique = true),
   Index(value = ["publishedOn"]),
   Index(value = ["updatedOn"])
 ])
 data class FeedItem constructor(
-    @PrimaryKey val id: Int,
-    @ColumnInfo val categoryId: Int,
+    val id: Int,
+    val categoryId: Int,
     @ColumnInfo val imageUrl: String?,
     @ColumnInfo val publishedOn: LocalDateTime,
     @ColumnInfo val updatedOn: LocalDateTime,
