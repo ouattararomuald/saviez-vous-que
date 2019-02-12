f27c9a1754326b34962c40c40b640942498aa92e
Ouattara Romuald <ouattararomuald@yahoo.fr>
2019-02-12 04:58:39 +0100

    minSdk 21

--- app/src/main/java/com/ouattararomuald/saviezvousque/posts/theme/ThemeDialogPicker.kt
+++ app/src/main/java/com/ouattararomuald/saviezvousque/posts/theme/ThemeDialogPicker.kt
@@ -10,9 +10,13 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Section
 import com.xwray.groupie.ViewHolder
 
-internal class ThemeDialogPicker(private val context: Context, onThemeSelectedPicker : (themeStyle: Int, themeName: String) -> Unit) {
+internal class ThemeDialogPicker(
+  private val context: Context,
+  onThemeSelectedPicker: (themeStyle: Int, themeName: String) -> Unit
+) {
 
-  private var themeNames: Array<String> = context.resources.getStringArray(R.array.theme_name_choice_array)
+  private var themeNames: Array<String> =
+      context.resources.getStringArray(R.array.theme_name_choice_array)
   private var themeColorsArrays: Array<Array<String>> = arrayOf(
       context.resources.getStringArray(R.array.blue_colors_choice_array),
       context.resources.getStringArray(R.array.cyan_colors_choice_array),
@@ -28,27 +32,35 @@ internal class ThemeDialogPicker(private val context: Context, onThemeSelectedPi
 
   private val dialogView = LayoutInflater.from(context)
       .inflate(R.layout.theme_selector_dialog, null, false)
-  private val themesRecyclerView = dialogView.findViewById<RecyclerView>(R.id.themesRecyclerView).apply {
-    layoutManager = LinearLayoutManager(context)
-    setHasFixedSize(true)
-    adapter = themesGroupAdapter
-  }
+  private val themesRecyclerView =
+      dialogView.findViewById<RecyclerView>(R.id.themesRecyclerView).apply {
+        layoutManager = LinearLayoutManager(context)
+        setHasFixedSize(true)
+        adapter = themesGroupAdapter
+      }
 
   private val alertDialogBuilder = AlertDialog.Builder(context).apply {
-    setTitle("Select a Theme")
+    setTitle(context.getString(R.string.select_a_theme))
     setView(dialogView)
   }
   private val alertDialog = alertDialogBuilder.create()
 
   init {
     themeDatas = arrayOf(
-        ThemeData(themeNames[0], themeColorsArrays[0][0], themeColorsArrays[0][1], themeColorsArrays[0][2]),
-        ThemeData(themeNames[1], themeColorsArrays[1][0], themeColorsArrays[1][1], themeColorsArrays[1][2]),
-        ThemeData(themeNames[2], themeColorsArrays[2][0], themeColorsArrays[2][1], themeColorsArrays[2][2]),
-        ThemeData(themeNames[3], themeColorsArrays[3][0], themeColorsArrays[3][1], themeColorsArrays[3][2]),
-        ThemeData(themeNames[4], themeColorsArrays[4][0], themeColorsArrays[4][1], themeColorsArrays[4][2]),
-        ThemeData(themeNames[5], themeColorsArrays[5][0], themeColorsArrays[5][1], themeColorsArrays[5][2]),
-        ThemeData(themeNames[6], themeColorsArrays[6][0], themeColorsArrays[6][1], themeColorsArrays[6][2])
+        ThemeData(themeNames[0], themeColorsArrays[0][0], themeColorsArrays[0][1],
+            themeColorsArrays[0][2]),
+        ThemeData(themeNames[1], themeColorsArrays[1][0], themeColorsArrays[1][1],
+            themeColorsArrays[1][2]),
+        ThemeData(themeNames[2], themeColorsArrays[2][0], themeColorsArrays[2][1],
+            themeColorsArrays[2][2]),
+        ThemeData(themeNames[3], themeColorsArrays[3][0], themeColorsArrays[3][1],
+            themeColorsArrays[3][2]),
+        ThemeData(themeNames[4], themeColorsArrays[4][0], themeColorsArrays[4][1],
+            themeColorsArrays[4][2]),
+        ThemeData(themeNames[5], themeColorsArrays[5][0], themeColorsArrays[5][1],
+            themeColorsArrays[5][2]),
+        ThemeData(themeNames[6], themeColorsArrays[6][0], themeColorsArrays[6][1],
+            themeColorsArrays[6][2])
     )
     themesSection.addAll(themeDatas.map {
       ThemeItem(context, it)
