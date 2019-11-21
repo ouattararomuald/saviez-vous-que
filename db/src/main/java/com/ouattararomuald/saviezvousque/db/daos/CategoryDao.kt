76e8ac7e8238ebe4e2618c2b5e8168cd17185820
Romuald OUATTARA <rouattar@bouyguestelecom.fr>
2019-11-21 17:30:11 +0100

    Add tests for Category Dao

--- db/src/main/java/com/ouattararomuald/saviezvousque/db/daos/CategoryDao.kt
+++ db/src/main/java/com/ouattararomuald/saviezvousque/db/daos/CategoryDao.kt
@@ -4,33 +4,66 @@ import com.ouattararomuald.saviezvousque.db.Category
 import com.ouattararomuald.saviezvousque.db.CategoryIdAndName
 import com.ouattararomuald.saviezvousque.db.CategoryQueries
 import com.ouattararomuald.saviezvousque.db.exceptions.CategoryIdNotFoundException
+import com.squareup.sqldelight.runtime.coroutines.asFlow
+import com.squareup.sqldelight.runtime.coroutines.mapToList
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class CategoryDao @Inject constructor(private val categoryQueries: CategoryQueries) {
 
-  fun getCategories(): List<CategoryIdAndName> = categoryQueries.categoryIdAndName().executeAsList()
+  fun getCategories(): Flow<List<CategoryIdAndName>> = categoryQueries.categoryIdAndName().asFlow().mapToList()
 
+  /**
+   * Creates all the given [categories].
+   *
+   * Existing categories won't be created.
+   *
+   * @param categories list of categories to create.
+   */
   fun createCategories(categories: List<Category>) {
     categories.forEach { category -> createCategory(category) }
   }
 
+  /**
+   * Creates the given [category] if it does not exists.
+   *
+   * @param category value for category.
+   */
   fun createCategory(category: Category) {
     if (!categoryExists(category.id)) {
       categoryQueries.createCategory(category.id, category.numberOfItems, category.name,
           category.slug)
-    } else {
-      updateCategory(category.id, category)
     }
   }
 
+  /**
+   * Updates the [Category] with the given [categoryId].
+   *
+   * @param categoryId id of the [Category] to update.
+   * @param newCategory new value for the category.
+   * @throws CategoryIdNotFoundException if no [Category] with the given [categoryId] has been found.
+   */
+  @Throws(CategoryIdNotFoundException::class)
   fun updateCategory(categoryId: Int, newCategory: Category) {
     if (categoryExists(categoryId)) {
-      categoryQueries.updateCategoryWithId(newCategory.name, newCategory.slug,
-          newCategory.numberOfItems, categoryId)
+      updateCategoryItem(categoryId, newCategory)
     } else {
       throw CategoryIdNotFoundException(categoryId)
     }
   }
 
-  private fun categoryExists(categoryId: Int): Boolean = categoryQueries.countCategoryWithId(categoryId).executeAsOne() > 0
+  private fun updateCategoryItem(categoryId: Int, category: Category) {
+    categoryQueries.updateCategoryWithId(category.name, category.slug, category.numberOfItems,
+        categoryId)
+  }
+
+  /** Deletes all categories. */
+  fun deleteCategories() {
+    categoryQueries.deleteCategories()
+  }
+
+  fun categoryExists(category: Category): Boolean = categoryExists(category.id)
+
+  fun categoryExists(categoryId: Int): Boolean = categoryQueries.countCategoryWithId(
+      categoryId).executeAsOne() > 0
 }
---
+++ db/src/test/java/com/ouattararomuald/saviezvousque/db/daos/CategoryDaoTest.kt
@@ -0,0 +1,148 @@
+package com.ouattararomuald.saviezvousque.db.daos
+
+import com.ouattararomuald.saviezvousque.db.Category
+import com.ouattararomuald.saviezvousque.db.CategoryQueries
+import com.ouattararomuald.saviezvousque.db.Database
+import com.ouattararomuald.saviezvousque.db.Post
+import com.ouattararomuald.saviezvousque.db.adapters.LocalDateTimeAdapter
+import com.ouattararomuald.saviezvousque.db.exceptions.CategoryIdNotFoundException
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.never
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+
+class CategoryDaoTest {
+
+  private val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+  private val dateAdapter = LocalDateTimeAdapter()
+
+  private lateinit var categoryQueries: CategoryQueries
+  private lateinit var categoryDao: CategoryDao
+
+  @Before
+  fun setUp() {
+    Database.Schema.create(driver)
+    val database = Database(driver, Post.Adapter(dateAdapter, dateAdapter))
+
+    categoryQueries = spy(database.categoryQueries)
+    categoryDao = CategoryDao(categoryQueries)
+  }
+
+  @After
+  fun tearDown() {
+  }
+
+  @Test
+  fun `verify calls for getCategories()`() {
+    categoryDao.getCategories()
+
+    verify(categoryQueries, times(1)).categoryIdAndName()
+  }
+
+  @Test
+  fun `verify calls for createCategory() for new category`() {
+    val category = createCategory(id = 1)
+
+    categoryDao.createCategory(category)
+
+    verify(categoryQueries, times(1)).countCategoryWithId(categoryId = category.id)
+    verify(categoryQueries, times(1)).createCategory(category.id, category.numberOfItems,
+        category.name, category.slug)
+    verify(categoryQueries, never()).updateCategoryWithId(anyString(), anyString(), anyInt(),
+        anyInt())
+  }
+
+  @Test
+  fun `verify calls for createCategory() for existing category`() {
+    val category = createCategory(id = 1)
+
+    categoryDao.createCategory(category)
+    categoryDao.createCategory(category)
+
+    verify(categoryQueries, times(2)).countCategoryWithId(categoryId = category.id)
+    verify(categoryQueries, times(1)).createCategory(category.id, category.numberOfItems,
+        category.name, category.slug)
+    verify(categoryQueries, never()).updateCategoryWithId(anyString(), anyString(), anyInt(),
+        anyInt())
+  }
+
+  @Test
+  fun `createCategories() should delegate to createCategory()`() {
+    val categories = listOf(createCategory(id = 1), createCategory(id = 1), createCategory(id = 1))
+
+    val spyCategoryDao = spy(categoryDao)
+    spyCategoryDao.createCategories(categories)
+
+    verify(spyCategoryDao, times(1)).createCategories(categories)
+    verify(spyCategoryDao, times(categories.size)).createCategory(category = categories.first())
+  }
+
+  @Test
+  fun `update existing category should success`() {
+    val category = createCategory(id = 1)
+
+    categoryDao.createCategory(category)
+    categoryDao.updateCategory(category.id, category)
+
+    verify(categoryQueries, times(2)).countCategoryWithId(categoryId = category.id)
+    verify(categoryQueries, times(1)).updateCategoryWithId(category.name, category.slug,
+        category.numberOfItems, category.id)
+  }
+
+  @Test
+  fun `verify calls for categoryExists()`() {
+    val category = createCategory(id = 2)
+
+    val spyCategoryDao = spy(categoryDao)
+
+    spyCategoryDao.categoryExists(category)
+
+    verify(categoryQueries, times(1)).countCategoryWithId(categoryId = category.id)
+    verify(spyCategoryDao, times(1)).categoryExists(category.id)
+  }
+
+  @Test(expected = CategoryIdNotFoundException::class)
+  fun `update on not existing category should fail`() {
+    val category = createCategory(id = 1)
+
+    categoryDao.updateCategory(category.id, category)
+  }
+
+  @Test
+  fun `verify calls for deleteCategories()`() {
+    categoryDao.deleteCategories()
+
+    verify(categoryQueries, never()).countCategoryWithId(categoryId = anyInt())
+    verify(categoryQueries, times(1)).deleteCategories()
+  }
+
+  private fun createCategories(quantity: Int): List<Category> {
+    val categories = mutableListOf<Category>()
+    repeat(quantity) {
+      val category = createCategory(id = it)
+      categories.add(category)
+    }
+
+    return categories
+  }
+
+  private fun createCategory(id: Int): Category {
+    return object : Category {
+      override val id: Int
+        get() = id
+      override val numberOfItems: Int
+        get() = id * 10
+      override val name: String
+        get() = "Category $id"
+      override val slug: String
+        get() = "category-slug-$id"
+    }
+  }
+}
